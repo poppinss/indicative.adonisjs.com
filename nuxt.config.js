47b5593f037db42f8ba00776cf8ef568672b3c46
Harminder virk <virk.officials@gmail.com>
2019-07-09 22:25:11 +0530

    refactor: add meta description,title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ module.exports = {
         keys: [
           {
             key: 'DIMER_BASE_URL',
-            default: 'http://localhost:5000'
+            default: 'http://192.168.1.7:5000'
           },
           {
             key: 'NODE_ENV',
@@ -33,5 +33,17 @@ module.exports = {
     color: '#078161'
   },
   plugins: ['~plugins/dimer.js', '~js/prism.js'],
-  css: ['~css/main.css', '~css/prism.css', '~css/dimer-sans/style.css']
+  css: ['~css/main.css', '~css/prism.css', '~css/dimer-sans/style.css'],
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+      { rel: 'icon', href: '/favicon-32x32.png', sizes: '32x32', type: 'png' },
+      { rel: 'icon', href: '/favicon-16x16.png', sizes: '16x16', type: 'png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+    ]
+  }
 }
--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import { routes } from 'dimer-vue'
+import * as mToString from 'mdast-util-to-string'
 
 import LayoutComponent from '~/components/Layout'
 import DocComponent from '~/components/Doc'
@@ -9,6 +10,27 @@ Vue.use(Router)
 
 DocComponent.pageOptions = {
   scrollToTop: true,
+  head () {
+    let description = this.doc.seoSummary
+
+    /**
+     * Set description as text from the first paragraph when `seoSummary`
+     * is missing.
+     */
+    if (!description) {
+      const firstP = this.doc.content.children.find((node) => node.tag === 'p')
+      if (firstP) {
+        description = mToString(firstP)
+      }
+    }
+
+    return {
+      title: this.doc.title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+      ],
+    }
+  }
 }
 
 export function createRouter (ssrContext, createDefaultRouter) {
